fbc2126928d79786e022dcc332cb6f8235cfd046
Ernesto <theflaco0831@gmail.com>
2026-03-01 00:42:49 +0100

    agregar package.json

--- Nuevo Python Script.py
+++
@@ -1,7 +0,0 @@
-echo "# convertidor-opus-mp3" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/theflaco0831-netizen/convertidor-opus-mp3.git
-git push -u origin main
--- README.js.py
+++
@@ -1,3 +0,0 @@
-git remote add origin https://github.com/theflaco0831-netizen/convertidor-opus-mp3.git
-git branch -M main
-git push -u origin main
